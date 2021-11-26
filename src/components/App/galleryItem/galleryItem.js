bd65c81aa980122fc2b92dd94368531dfc608627
Caleb Hatch <hatchcaleb@gmail.com>
2021-11-26 12:03:48 -0600

    styled discs

--- src/components/App/galleryItem/galleryItem.js
+++ src/components/App/galleryItem/galleryItem.js
@@ -1,27 +1,33 @@
 import { useState } from "react";
+import "./galleryItem.css";
 
 function GalleryItem({ disc }) {
-  const [showDescription, setShowDes] = useState(disc.description);
+  const [showDescription, setShowDescription] = useState(false);
   const [likeCount, setLikeCount] = useState(0);
 
-//   const displayText = () => {
-    
-//   };
+  const toggleDescription = () => {
+    console.log("working click");
+    setShowDescription(!showDescription);
+  };
+
+  const displayItems = () => {
+    if (showDescription) {
+      return <p onClick={toggleDescription}>{disc.description}</p>;
+    } else {
+      return <img key={disc.id} src={disc.path} onClick={toggleDescription} />;
+    }
+  };
 
   const likes = () => {
     setLikeCount(likeCount + 1);
-  }
+  };
 
   return (
-    <tbody>
-      <tr>
-        <td>
-          <img key={disc.id} src={disc.path} />
-          <td>Likes: {likeCount}</td>
-          <button onClick={likes}>likes</button>
-        </td>
-      </tr>
-    </tbody>
+    <div className="disc">
+      {displayItems()}
+      <p>Likes: {likeCount}</p>
+      <button onClick={likes}>likes</button>
+    </div>
   );
 }
 
--- src/components/App/galleryList/galleryList.js
+++ src/components/App/galleryList/galleryList.js
@@ -1,14 +1,13 @@
 import GalleryItem from "../galleryItem/galleryItem";
 
+
 function GalleryList({ galleryList }) {
   return (
     <div>
       <p>My Discs</p>
-      <table>
         {galleryList.map((disc) => {
           return <GalleryItem disc={disc} />;
         })}
-      </table>
     </div>
   );
 }
